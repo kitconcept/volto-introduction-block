4ee8da7a55b23f5b58eccb53cdb24726e41f2002
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-01-24 18:48:59 -0600

    slateSettings customization proof of concept

--- src/components/Blocks/Introduction/Edit.jsx
+++ src/components/Blocks/Introduction/Edit.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 import { SidebarPortal } from '@plone/volto/components';
 import Sidebar from './Sidebar';
 import { DetachedTextBlockEditor } from '@plone/volto-slate/blocks/Text/DetachedTextBlockEditor';
+import config from '@plone/volto/registry';
 
 const IntroductionBlockEdit = (props) => {
   const { data, onChangeBlock, block, selected } = props;
 
+  const filteredToolbarProps = {
+    ...props,
+    slateSettings: {
+      ...config.settings.slate,
+      toolbarButtons: [
+        'bold',
+        'italic',
+        'strikethrough',
+        'link',
+        'separator',
+        'sub',
+        'sup',
+        'separator',
+        'numbered-list',
+        'bulleted-list',
+      ],
+    },
+  };
+
   return (
     <>
-      <DetachedTextBlockEditor {...props} />
+      <DetachedTextBlockEditor {...filteredToolbarProps} />
       <SidebarPortal selected={selected}>
         <Sidebar
           {...props}
